79684699057703edd7651d820b2eb5a2237901d3
LuisP <jluisp.m.19@ciencias.unam.mx>
2024-02-18 00:14:54 -0600

    agregado de variables en clase suscriptor

--- Suscriptor.java
+++ Suscriptor.java
@@ -1,4 +1,47 @@
+
+/**
+ * 
+ * 
+ * @author Mata
+ * @author Hermes
+ * @author Steve
+ * 
+ */
 public class Suscriptor implements Observador{
+
+  private String nombre;
+
+  private double saldo;
+
+  private ServicioStreaming servicio;
+
+  private int antiguedadMemeflix;
+
+  private int antiguedadMomazon;
+
+  private int antiguedadSpootify;
+
+  private int antiguedadHVO;
+
+  private int antiguedadThisney;
+
+  private String tipoDeSuscripcionMemeflix;
+
+  private String tipoDeSuscripcionMomazon;
+
+  private String tipoDeSuscripcionSpootify;
+
+  private String tipoDeSuscripcionHVO;
+
+  private String tipoDeSuscripcionThisney;
+
+  public Suscriptor(String nombre, double saldo){
+
+    this.nombre = nombre;
+
+    this.saldo = saldo;
+
+  }
 
   public void actualizar() {
     // TODO Auto-generated method stub
