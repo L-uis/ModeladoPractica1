bfa8cbb5ad53917c65ef87adc23a5269220f9879
steve-quezada <80070698+steve-quezada@users.noreply.github.com>
2024-02-22 13:06:00 -0600

    javadoc Cobro

--- CobroThisney.java
+++ CobroThisney.java
@@ -12,6 +12,7 @@ public interface CobroThisney {
   /**
    * Metodo que realiza el cobro al suscriptor.
    * 
+   * @param cliente El cliente al que se le cobra el servicio.
    * @return el estado del cobro.
    */
   String cobro(Cliente cliente);
